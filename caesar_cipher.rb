4e790babaa7060dd5f9e50831b4165bbdc729850
JorisLespagnol <46922441+Bwerpophile@users.noreply.github.com.>
2025-01-28 17:08:47 +0100

    Starting the exercise, generate an alphabet inside an array, trying to figure out what comes next

---
+++ caesar_cipher.rb
@@ -0,0 +1,11 @@
+alphabet = [*('a'..'z')]
+
+test_string = "Hello World"
+
+# il faut comprendre la phrase
+# puis la comparer à l'array pour obtenir un numéro correspondant à la lettre, 
+# puis ajouter 3 et recréer la phrase
+
+def sentence_to_number(sentence) 
+  
+end
